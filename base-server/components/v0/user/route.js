5d493482e27df1df136157217258eccbf4c00e7e
indrapal <indrapal.yadav@innoplexus.com>
2016-03-17 16:25:39 +0530

    implement request validator

--- base-server/components/v0/user/route.js
+++ base-server/components/v0/user/route.js
@@ -4,7 +4,8 @@ var express = require('express');
 
 var userController = require('./controller');
 var userRouter = express.Router();
-
-userRouter.post('/', userController.createUser);
+var paperwork = require('paperwork');
+var validator = require('../../middleware/validator').validator;
+userRouter.post('/',paperwork.accept(validator.createUser), userController.createUser);
 
 module.exports = userRouter;
--- base-server/middleware/validator.js
+++ base-server/middleware/validator.js
@@ -1,6 +1,18 @@
 'use strict'
 
-
-var validator = {};
+var paperwork = require('paperwork');
+var validator = {
+  createUser: {
+    firstname: /[a-zA-Z]+/,
+    lastname: /[a-zA-Z]+/,
+    email: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+    contact: String,
+    role: [String],
+    username: String,
+    hashedPassword: String,
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
+  }
+};
 
 module.exports = validator;
